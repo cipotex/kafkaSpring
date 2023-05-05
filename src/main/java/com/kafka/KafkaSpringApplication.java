6c01de02085375bda42840416c1ba1f30e22053a
cipotex <cipotex@hotmail.com>
2023-05-04 22:10:02 -0600

    actualizacion de proyecto

--- src/main/java/com/kafka/KafkaSpringApplication.java
+++ src/main/java/com/kafka/KafkaSpringApplication.java
@@ -1,13 +1,41 @@
 package com.kafka;
 
+import java.util.concurrent.CompletableFuture;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFuture;
 
 @SpringBootApplication
-public class KafkaSpringApplication {
+public class KafkaSpringApplication implements CommandLineRunner {
 
+	private final static Logger LOG = LoggerFactory.getLogger(KafkaSpringApplication.class);
+	
+	@Autowired
+	private KafkaTemplate<Integer, String> kafkaTemplate;
+
+	
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaSpringApplication.class, args);
 	}
 
-}
+	@KafkaListener(topics ="devs4j-topic", groupId ="consumer")
+	public void listen(String message) {
+		LOG.info("Received Messasge in group consumer {} ", message);
+	}
+	
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+				kafkaTemplate.send("devs4j-topic","Sample message ").get();
+	
+	}
+	}
--- src/main/java/com/kafka/configutarion/KafkaConfigutation.java
+++ src/main/java/com/kafka/configutarion/KafkaConfigutation.java
@@ -4,21 +4,27 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.common.serialization.IntegerDeserializer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.IntegerSerializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 
+@Configuration
 public class KafkaConfigutation {
 
 	@Bean
 	public Map<String, Object> consumerProprties() {
 		Map<String, Object> props = new HashMap<>();
 		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-		props.put(ConsumerConfig.GROUP_ID_CONFIG, "group");
+		props.put(ConsumerConfig.GROUP_ID_CONFIG, "dev4j-group");
 		props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
 		props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "100");
 		props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "15000");
@@ -30,7 +36,6 @@ public class KafkaConfigutation {
 	@Bean
 	public ConsumerFactory<String, String> consumerFactory() {
 		return new DefaultKafkaConsumerFactory<>(consumerProprties());
-
 	}
 
 	@Bean
@@ -40,10 +45,26 @@ public class KafkaConfigutation {
 		return factory;
 	}
 
+	///  *******************************
+	public Map<String, Object> producerProps() {
+		Map<String, Object> props=new HashMap<>();
+		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		//props.put(ProducerConfig.RETRIES_CONFIG, 0);
+		//props.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
+		//props.put(ProducerConfig.LINGER_MS_CONFIG, 1);
+		//props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432);
+		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class);
+		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		return props;
+	}
 	
-	@KafkaListener(topics ="devs4j-topic", groupId ="consumer")
-	public void listen(String message) {
-		System.out.println("Received Messasge in group foo"+message);
+	@Bean
+	public KafkaTemplate<Integer, String> createTemplate() { 
+		Map<String, Object>senderProps= producerProps();
+		ProducerFactory<Integer, String> pf= new DefaultKafkaProducerFactory<Integer, String>(senderProps);
+		KafkaTemplate<Integer, String> template=new KafkaTemplate<>(pf);
+		return template;
 	}
 
+	
 }
